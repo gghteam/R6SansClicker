b233dededac641d14cdaf13a0b73b4b539dc81c5
hanseungtheshiba <ggm.t200019@ggh.goe.go.kr>
2020-10-15 14:25:09 +0900

    10월 15일 5교시 수업

---
+++ Assets/Scripts/Base/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Player
+{
+    public long union;
+    public long bobBurger;
+    public Pants[] pants;
+}
+
+[Serializable]
+public class Pants
+{
+    public long level;
+    public long upgradeCost;
+    public long unionPerSecond;
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Player player;
+
+    private string jsonString = "";
+    private void Awake()
+    {
+        player = new Player();
+    }
+
+    public void OnClickSave()
+    {
+        jsonString = JsonUtility.ToJson(player);
+        Debug.Log("JSON : " + jsonString);
+    }
+
+    public void OnClickLoad() 
+    {
+        player = JsonUtility.FromJson<Player>(jsonString);
+    }
+}
